4ce8d3fcfdbb718ab365898fac9cb9576b15dbde
Cece <choush@msudenver.edu>
2025-04-26 16:17:17 -0600

    starting page for chatbot

---
+++ lucent/lib/themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF242325);
+  static const Color white = Colors.white;
+  // for more colors 
+}
