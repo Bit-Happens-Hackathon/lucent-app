16953255d2885349e3d5700a24b8f0b722cd3fb3
Cece <choush@msudenver.edu>
2025-04-26 23:36:18 -0600

    clickable wellness stats with space for graph

--- lucent/lib/widgets/wellness_card.dart
+++ lucent/lib/widgets/wellness_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../themes.dart';
 
-class WellnessCard extends StatelessWidget {
+class WellnessCard extends StatefulWidget {
   final String category;
   final int percentage;
 
@@ -12,37 +12,140 @@ class WellnessCard extends StatelessWidget {
   });
 
   @override
+  State<WellnessCard> createState() => _WellnessCardState();
+}
+
+class _WellnessCardState extends State<WellnessCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.95).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _showDetailsModal() {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true, 
+        backgroundColor: AppColors.background,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        builder: (context) {
+          final double screenWidth = MediaQuery.of(context).size.width;
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    widget.category,
+                    style: const TextStyle(
+                      color: AppColors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    "${widget.percentage}%",
+                    style: const TextStyle(
+                      color: AppColors.primaryBlue,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Container(
+                    width: screenWidth,
+                    height: screenWidth,
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryBlue.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Graph goes here',
+                        style: TextStyle(color: AppColors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      margin: const EdgeInsets.symmetric(horizontal: 6.0),
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: AppColors.primaryBlue,
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            category,
-            style: const TextStyle(
-              color: AppColors.background,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
+    return GestureDetector(
+      onTapDown: (_) => _controller.forward(),
+      onTapUp: (_) {
+        _controller.reverse();
+        _showDetailsModal();
+      },
+      onTapCancel: () => _controller.reverse(),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) => Transform.scale(
+          scale: _scaleAnimation.value,
+          child: child,
+        ),
+        child: Container(
+          width: 170,
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: AppColors.primaryBlue,
+            borderRadius: BorderRadius.circular(16.0),
           ),
-          const SizedBox(height: 8),
-          Text(
-            "$percentage%",
-            style: const TextStyle(
-              color: AppColors.background,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.category,
+                style: const TextStyle(
+                  color: AppColors.background,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "${widget.percentage}%",
+                style: const TextStyle(
+                  color: AppColors.background,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
